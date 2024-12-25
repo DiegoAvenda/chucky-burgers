b3205994eefaa2edb7795708bb4e75106641cd02
Diego Avendaño <75541249+DiegoAvenda@users.noreply.github.com>
2024-12-24 21:01:37 -0600

    show order day

--- src/routes/profile/+page.server.js
+++ src/routes/profile/+page.server.js
@@ -26,6 +26,8 @@ export const load = async ({ locals, url }) => {
 		const orders = rawOrders.map((order) => ({
 			...order,
 			createdAt: order.createdAt.toLocaleTimeString('en-US', {
+				month: '2-digit',
+				day: '2-digit',
 				hour: '2-digit',
 				minute: '2-digit',
 				hour12: true,
